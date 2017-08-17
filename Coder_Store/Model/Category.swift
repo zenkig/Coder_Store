7b511e3e121f9988ef8fb10d8510995e546b1d7a
Zenkig Gou <zenkig@126.com>
2017-08-17 17:30:10 +0800

    new color updates

---
+++ Coder_Store/Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Coder_Store
+//
+//  Created by goujingxiang on 17/08/2017.
+//  Copyright © 2017 zenkig. All rights reserved.
+//
+
+import Foundation
---
+++ Coder_Store/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  Coder_Store
+//
+//  Created by goujingxiang on 17/08/2017.
+//  Copyright © 2017 zenkig. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
